41a7b5e629918bea8905a6edc763c30a9ff2eddd
EJ Marshall <emarshall9247@stu.d214.org>
2026-04-17 11:08:16 -0500

    I added the views for the games I am creating

---
+++ War.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  HallOfGames
+//
+//  Created by Evan J. Marshall on 4/17/26.
+//
+
+import SwiftUI
+struct war: View {
+    var body: some View {
+        Text("War, the card game")
+    }
+}
+
+#Preview {
+    war()
+}

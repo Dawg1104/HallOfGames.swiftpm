c7f3f780ebfe308e108426f53aba0999be4d48e1
Niko D <moki.gaming12@gmail.com>
2026-04-21 11:08:21 -0500

    Made scroll view with all the games in it.

--- Menu.swift
+++ Menu.swift
@@ -8,7 +8,29 @@
 import SwiftUI
 
 struct Menu: View {
+    let games = ["BlackJack","FiveNightsAtJobApplications","JumpyBird","Pong","PopTheLock","RNG","Roulette","War"]
     var body: some View {
-        Text("Menu")
+        
+        ZStack {
+            Image("TheHall")
+                .resizable()
+            
+            
+            ScrollView {
+                VStack {
+                    ForEach(games, id: \.self) { games in
+                        NavigationLink(destination: BlackJack()) {
+                            Text(games)
+                                .foregroundStyle(Color.white)
+                                .padding()
+                                .frame(maxWidth: 200)
+                                .background(Color.gray.opacity(0.2))
+                                .clipShape(Capsule())
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
 }

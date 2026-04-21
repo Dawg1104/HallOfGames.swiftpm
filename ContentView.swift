d98c429f9094241183a59f102058061008165d4e
Niko D <moki.gaming12@gmail.com>
2026-04-21 10:42:32 -0500

    Finished title screen, made button a capsule shape.

--- ContentView.swift
+++ ContentView.swift
@@ -3,20 +3,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text("Hall of Games")
-            .font(.largeTitle)
-        
         NavigationStack {
-        ZStack {
-            Image("TheHall")
-                .resizable()
-            VStack {
-                
-                NavigationLink("Go to menu") {
-                    Menu()
+            ZStack {
+                Image("TheHall")
+                    .resizable()
+                VStack {
+                    
+                    Text("Hall of Games")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.white)
+                    
+                    NavigationLink(destination: Menu()) {
+                        Text("Menu")
+                                .padding()
+                                .frame(maxWidth: 100, maxHeight: 40)
+                                .background(Color.orange)
+                                .foregroundColor(.white)
+                                .clipShape(Capsule())
+                    }
+                    .foregroundStyle(Color.white)
                 }
-                Text("Hall of Games")
-                    .font(.largeTitle)
             }
         }
     }

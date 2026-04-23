cdfe490803a6a79f13bb624b303094c1c089b294
Niko D <moki.gaming12@gmail.com>
2026-04-23 11:12:24 -0500

    Made prototype for JumpyBird.

--- JumpyBird.swift
+++ JumpyBird.swift
@@ -8,7 +8,50 @@
 import SwiftUI
 
 struct JumpyBird: View {
+    
+    @State var birdY: CGFloat = 300
+    @State var velocity: CGFloat = 0
+    @State var pipeX: CGFloat = 400
+    
+    let gravity: CGFloat = 0.5
+    let jump: CGFloat = -8
+    
     var body: some View {
-        Text("JumpyBird")
+        
+        ZStack {
+            Color.blue.ignoresSafeArea()
+                .onTapGesture {
+                    velocity = jump
+                }
+            
+            Circle()
+                .frame(width: 40, height: 40)
+                .position(x: 100, y: birdY)
+            
+            
+            Rectangle()
+                .frame(width: 60, height: 300)
+                .position(x: pipeX, y: 400)
+            
+            
+                
+            
+            
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { _ in
+                        
+                        velocity += gravity
+                        birdY += velocity
+                        
+                        pipeX -= 3
+                        
+                        if pipeX < -50 {
+                            pipeX = 400
+                        }
+                    }
+                }
+        }
     }
+    
 }
+

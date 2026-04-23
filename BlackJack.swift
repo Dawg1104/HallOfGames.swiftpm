1687b894fbd7f003dc319439597eeaa703cdb1aa
EJ Marshall <emarshall9247@stu.d214.org>
2026-04-23 11:08:37 -0500

    I added the player score

--- BlackJack.swift
+++ BlackJack.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 struct BlackJack: View {
     @State var playerScore = 0
+    @State private var isDisabled: Bool = false
     @State var computerScore = 0
     @State var cardImage = ""
     @State var number = 0
+    @State var endGameDisplay = ""
     var body: some View {
         ZStack {
             Image("card table")
@@ -19,6 +21,7 @@ struct BlackJack: View {
             VStack {
                 Text("computer score: \(computerScore)")
                     .font(.system(size: 30, weight: .heavy, design: .monospaced))
+                Text(endGameDisplay)
                 HStack {
                     Image("deckOfCards")
                         .resizable()
@@ -27,19 +30,37 @@ struct BlackJack: View {
                         .resizable()
                         .frame(width: 100, height: 200)
                 }
+                Text("Player Score: \(playerScore)")
+                    .font(.system(size: 30, weight: .heavy, design: .monospaced))
                 Button {
+                    isDisabled = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            isDisabled = false
                     number = Int.random(in: 1...10)
                     switch number {
                     case 1: cardImage = "AceOfHearts"
-                        number += 1
+                        playerScore += 1
+                    case 2: cardImage = "2OfHearts"
+                        playerScore += 2
+                        case 3: cardImage = "3OfHearts"
+                        playerScore += 3
+                        case 4: cardImage = "4OfHearts"
+                        playerScore += 4
                     default: cardImage = "AceOfHearts"
-                        number += 1
+                        playerScore += 1
+                    }
+                    if playerScore > 21 {
+                        playerScore = 0
+                        endGameDisplay = "BUST"
+                       
+            }
                     }
                 } label: {
                     Image("BHIT")
                         .resizable()
                         .frame(width: 200, height: 125)
                 }
+                .disabled(isDisabled)
                 Button {
                     
                 } label: {

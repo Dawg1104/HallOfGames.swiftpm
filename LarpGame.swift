1ca8652736c80438f179e1da0671c6ba23b32efe
AK <kingdodgemangay@gmail.com>
2026-04-27 11:16:57 -0500

    LarpGame worked on

--- LarpGame.swift
+++ LarpGame.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct LarpGame: View {
+    @State var larps: Int = 0
+    @State var autolarpersowned: Int = 0
+    @State var larpsahursowned: Int = 0
     var body: some View {
-        HStack {
-            
-            VStack{
+        VStack {
+            Text("Larp Clicker")
+                .font(.largeTitle)
+            Text("Larps: \(larps)")
+            HStack {
+                Button {
+                    larps += 1
+                } label: {
+                    Image("Larp")
+                }
+
                 
+                VStack{
+                    Text("Auto-Larper")
+                        .font(.title)
+                Button("Buy Auto-Larper") {
+                     autolarpersowned += 1
+                    larps -= 10
+                    }
+                    Text("Larp Sahur")
+                        .font(.title)
+                    Button("Buy larp sahur") {
+                        larpsahursowned += 1
+                    }
+                }
             }
         }
     }
 }
-
 

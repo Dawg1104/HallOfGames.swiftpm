414336cc57529b2f0dfd6f6b68eaa3a5c0234bf2
AK <kingdodgemangay@gmail.com>
2026-04-29 10:25:13 -0500

    Made the per second system work

--- LarpGame.swift
+++ LarpGame.swift
@@ -12,7 +12,8 @@ struct LarpGame: View {
     @State var autolarpersowned: Int = 0
     @State var larpsahursowned: Int = 0
     @State var autolarpercost: Int = 10
-    
+    @State var larppsgain: Int = 0
+    @State var larpssahurcost: Int = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
@@ -26,27 +27,37 @@ struct LarpGame: View {
                 } label: {
                     Image("Larp")
                 }
-
+                
                 
                 VStack{
                     Text("Auto-Larper")
                         .font(.title)
-                Button("Buy Auto-Larper, cost \(autolarpercost)") {
-                    if larps >= autolarpercost {
-                        autolarpersowned += 1
-                        larps -= 10
-                        autolarpercost = Int(Double(autolarpercost) * 1.5)
-                    }
+                    Button("Buy Auto-Larper, cost \(autolarpercost)") {
+                        if larps >= autolarpercost {
+                            autolarpersowned += 1
+                            larps -= autolarpercost
+                            autolarpercost = Int(Double(autolarpercost) * 1.5)
+                            larppsgain += 1
+                        }
                     }
                     Text("Larp Sahur")
                         .font(.title)
-                    Button("Buy larp sahur") {
-                        larpsahursowned += 1
-                        larps -= 100
+                    Button("Buy larp sahur, cost \(larpssahurcost)") {
+                        if larps >= larpssahurcost {
+                            
+                            
+                            larpsahursowned += 1
+                            larps -= larpssahurcost
+                            larpssahurcost = Int(Double(larpssahurcost) * 1.5)
+                            larppsgain += 10
+                        }
                     }
                 }
             }
+            
+            .onReceive(timer) { _ in
+                larps += larppsgain}
         }
     }
+    
 }
-

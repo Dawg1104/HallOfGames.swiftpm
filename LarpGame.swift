19f8178ef2444b289986af4b326987b1f534fd3f
AK <kingdodgemangay@gmail.com>
2026-04-29 10:05:37 -0500

    Added many new fixes, including costs, and an eventual auto system

--- LarpGame.swift
+++ LarpGame.swift
@@ -11,6 +11,10 @@ struct LarpGame: View {
     @State var larps: Int = 0
     @State var autolarpersowned: Int = 0
     @State var larpsahursowned: Int = 0
+    @State var autolarpercost: Int = 10
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
             Text("Larp Clicker")
@@ -27,14 +31,18 @@ struct LarpGame: View {
                 VStack{
                     Text("Auto-Larper")
                         .font(.title)
-                Button("Buy Auto-Larper") {
-                     autolarpersowned += 1
-                    larps -= 10
+                Button("Buy Auto-Larper, cost \(autolarpercost)") {
+                    if larps >= autolarpercost {
+                        autolarpersowned += 1
+                        larps -= 10
+                        autolarpercost = Int(Double(autolarpercost) * 1.5)
+                    }
                     }
                     Text("Larp Sahur")
                         .font(.title)
                     Button("Buy larp sahur") {
                         larpsahursowned += 1
+                        larps -= 100
                     }
                 }
             }

c51b30a1103406f905b637fa0acada8f52e9fbff
AK <kingdodgemangay@gmail.com>
2026-04-29 11:18:21 -0500

    fixed

--- LarpGame.swift
+++ LarpGame.swift
@@ -16,7 +16,12 @@ struct LarpGame: View {
     @AppStorage("lsc") var larpssahurcost: Int = 100
     let lastOpen = Date()
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    
+    @AppStorage("OLO") var onlinelarpersowned: Int = 0
+    @AppStorage("OLC") var onlinelarperscost: Int = 1000
+    @AppStorage("PLO") var professionalowned: Int = 0
+    @AppStorage("PLC") var professionalcost: Int = 5000
+    @AppStorage("foe") var factoryowned: Int = 0
+    @AppStorage("foc") var factorycost: Int = 25000
     var body: some View {
         VStack {
             Text("Larp Clicker")
@@ -33,7 +38,9 @@ struct LarpGame: View {
                 
                 VStack{
                     HStack {
-                        
+                        Image("autolarp")
+                            .resizable()
+                            .frame(width: 50, height: 50)
                         Text("Auto-Larper")
                             .font(.title)
                     }
@@ -47,7 +54,9 @@ struct LarpGame: View {
                         }
                     }
                     HStack {
-                        
+                        Image ("larpsahur")
+                            .resizable()
+                            .frame(width: 50, height: 50)
                         
                         Text("Larp Sahur")
                             .font(.title)
@@ -63,17 +72,77 @@ struct LarpGame: View {
                             larppsgain += 10
                         }
                     }
+                    
+                    HStack {
+                        Image ("")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        
+                        Text("Online Larper")
+                            .font(.title)
+                    }
+                    Text("Owned: \(onlinelarpersowned)")
+                    Button("Buy larp sahur, cost \(onlinelarperscost)") {
+                        if larps >= onlinelarperscost {
+                            
+                            
+                            onlinelarpersowned += 1
+                            larps -= onlinelarperscost
+                            onlinelarperscost = Int(Double(onlinelarperscost) * 1.5)
+                            larppsgain += 100
+                        }
+                    }
+                    HStack {
+                        Image ("")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        
+                        Text("Professional Larper")
+                            .font(.title)
+                    }
+                    Text("Owned: \(professionalowned)")
+                    Button("Buy larp sahur, cost \(professionalcost)") {
+                        if larps >= professionalcost {
+                            
+                            
+                            professionalowned += 1
+                            larps -= professionalcost
+                            professionalcost = Int(Double(professionalcost) * 1.5)
+                            larppsgain += 500
+                        }
+                    }
+                    HStack {
+                        Image ("")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        
+                        Text("Larp Factory")
+                            .font(.title)
+                    }
+                    Text("Owned: \(factorycost)")
+                    Button("Buy larp sahur, cost \(factorycost)") {
+                        if larps >= factorycost {
+                            
+                            
+                            factoryowned += 1
+                            larps -= factorycost
+                            factorycost = Int(Double(factorycost) * 1.5)
+                            larppsgain += 250
+                        }
+                    }
                 }
-            }
+                }
+                }
             
+        
             .onReceive(timer) { _ in
                 larps += larppsgain}
             
         }
-    }
-    
-    
 }
+    
+    
+
 func format(_ num: Int) -> String {
     if num >= 1_000_000 {
         return "\(num / 1_000_000)M"
